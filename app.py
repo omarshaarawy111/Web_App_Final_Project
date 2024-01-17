031dc8c762a927d0927a4c1dccf77d6ba390ab9e
omarshaarawy111 <83236568+omarshaarawy111@users.noreply.github.com>
2024-01-17 19:39:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,6 @@ import streamlit as st
 #import libraries that is going to be used in our project 
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import plotly.express as px
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-import warnings
-warnings.filterwarnings('ignore')
 import pickle
 #function to authenticate user
 def authenticate_user(email, password):
