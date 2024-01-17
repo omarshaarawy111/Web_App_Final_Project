85247069a6d1427e0fe036e960308980863fc076
omarshaarawy111 <83236568+omarshaarawy111@users.noreply.github.com>
2024-01-17 19:07:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn
 #function to authenticate user
 def authenticate_user(email, password):
     #super admin credentials
